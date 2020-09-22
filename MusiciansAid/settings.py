8e130ccd43b257629f5b4e5bd5f09fb23cbb6a38
SATWIK-RAVULAPATI <ravulapati.saisatwik@gmail.com>
2020-09-22 16:47:14 +0530

    comit

--- MusiciansAid/settings.py
+++ MusiciansAid/settings.py
@@ -25,7 +25,7 @@ BASE_DIR =os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!2v4g)-s=2bjt08l62)6%1r9-!#7)q#c0l86+(ld@3%*%hvf$m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
